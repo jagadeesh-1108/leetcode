62a3ac1ad3e6d8726ea8a56699448562224fc5c1
Mannem Jagadeesh <jagadeshmannem@gmail.com>
2026-07-22 11:44:44 +0530

    Time: 0 ms (100%), Space: 19.2 MB (57.26%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -4,9 +4,7 @@ class Solution:
         s,s1=0,0
         if n%2==0 or n%2!=0:
             s = n*(n+1)
-            print(s)
             s1= n*n
-            print(s1)
         r=math.gcd(s,s1)
         return r
 
