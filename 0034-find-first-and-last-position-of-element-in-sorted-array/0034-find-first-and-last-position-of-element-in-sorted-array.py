85180e4d2f845fdfd57fe3ad005d9ba6f7c573e6
Mannem Jagadeesh <jagadeshmannem@gmail.com>
2026-08-11 18:28:06 +0530

    Time: 0 ms (100%), Space: 20.6 MB (59.81%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -3,31 +3,31 @@ class Solution:
         def first_position():
             low1= 0
             high1= len(nums)-1
-            ans1 = -1
+            val1 = -1
             while (low1<=high1):
                 mid = (low1 + high1) //2
                 if nums[mid] == target:
-                    ans1 = mid 
+                    val1 = mid 
                     high1 = mid -1
                 elif nums[mid] < target:
                     low1 = mid + 1
                 else:
                     high1 =  mid - 1
-            return ans1
+            return val1
         def second_position():
             low2 = 0
             high2 = len(nums)-1
-            ans2=-1
+            val2= -1
             while (low2<=high2):
                 mid = (low2 + high2) //2
                 if nums[mid] == target:
-                    ans2 = mid
+                    val2 = mid
                     low2 = mid + 1
                 elif nums[mid] < target:
                     low2 = mid + 1
                 else:
                     high2 =  mid - 1
-            return ans2
+            return val2
         return [first_position(),second_position()]
 
         
